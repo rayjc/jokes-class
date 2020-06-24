fa2eeae45ee9d6e0431b7743bd9dc60c0c3451db
Ray Chung <ray.j.chung@gmail.com>
2020-06-24 16:24:42 -0700

    Add localStorage and loading message.

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,78 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
-// function JokeList({ numJokesToGet = 10 }) {
-//   const [jokes, setJokes] = useState([]);
-
-//   /* get jokes if there are no jokes */
-
-//   useEffect(function() {
-//     async function getJokes() {
-//       let j = [...jokes];
-//       let seenJokes = new Set();
-//       try {
-//         while (j.length < numJokesToGet) {
-//           let res = await axios.get("https://icanhazdadjoke.com", {
-//             headers: { Accept: "application/json" }
-//           });
-//           let { status, ...jokeObj } = res.data;
-
-//           if (!seenJokes.has(jokeObj.id)) {
-//             seenJokes.add(jokeObj.id);
-//             j.push({ ...jokeObj, votes: 0 });
-//           } else {
-//             console.error("duplicate found!");
-//           }
-//         }
-//         setJokes(j);
-//       } catch (e) {
-//         console.log(e);
-//       }
-//     }
-
-//     if (jokes.length === 0) getJokes();
-//   }, [jokes, numJokesToGet]);
-
-//   /* empty joke list and then call getJokes */
-
-//   function generateNewJokes() {
-//     setJokes([]);
-//   }
-
-//   /* change vote for this id by delta (+1 or -1) */
-
-//   function vote(id, delta) {
-//     setJokes(allJokes =>
-//       allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-//     );
-//   }
-
-//   /* render: either loading spinner or list of sorted jokes. */
-
-//   if (jokes.length) {
-//     let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-
-//     return (
-//       <div className="JokeList">
-//         <button className="JokeList-getmore" onClick={generateNewJokes}>
-//           Get New Jokes
-//         </button>
-
-//         {sortedJokes.map(j => (
-//           <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-//         ))}
-//       </div>
-//     );
-//   }
-
-//   return null;
-
-// }
-
 class JokeList extends React.Component {
-  static defaultProps = { numJokesToGet: 10 };
+  static defaultProps = { numJokesToGet: 10, key: "jokes" };
   constructor(props) {
     super(props);
     this.state = { jokes: [] };
@@ -118,7 +50,12 @@ class JokeList extends React.Component {
   }
 
   async componentDidMount() {
-    this.getJokes();
+    const storedJokes = window.localStorage.getItem(this.props.key);
+    if (storedJokes) {
+      this.setState({ jokes: JSON.parse(storedJokes) })
+    } else {
+      this.getJokes();
+    }
   }
 
   async componentDidUpdate() {
@@ -133,6 +70,8 @@ class JokeList extends React.Component {
     if (jokes.length) {
       let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
 
+      window.localStorage.setItem(this.props.key, JSON.stringify(jokes));
+
       return (
         <div className="JokeList">
           <button className="JokeList-getmore" onClick={this.generateNewJokes}>
@@ -146,7 +85,7 @@ class JokeList extends React.Component {
       );
     }
 
-    return null;
+    return <h1>Loading...</h1>;
   }
 }
 
